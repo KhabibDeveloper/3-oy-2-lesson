bd2740602cf78b2d6f085fac14661188e263c70a
KhabibDeveloper <xabibullorixsiboyev8@gmail.com>
2022-11-15 20:10:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+alert("Assalomu alaykum");
+
+var userName = prompt("ismigizni kiriting");
+console.log(`Mening ismim ${userName}.`);
+
+var userfamilya = prompt("Familyangizni kiriting");
+console.log(`Mening familyam ${userfamilya}.`);
+
+var userAge = prompt("Yoshingiznini kiriting");
+
+console.log(`.Men ${userAge}daman.`);
+var userGroup = prompt("guruxingixni kiriting");
+
+console.log(`Guruxim ${userGroup}. `);
+var userTell = prompt("telefon raqamizni kiriting");
+
+console.log(`Telefon raqamim ${userTell}. `);
+var userEmail = prompt("emailadres kriting");
+
+console.log(`emailadres ${userEmail}. `);
+var userMW = prompt("jinsingizni kiriting");
+console.log(`Jinsim ${userMW}.`);
+
+var userColor = prompt("favorite color");
+
+console.log(`My favourite color ${userColor}.`);
+var userxulosa = prompt("xulosam");
+
+console.log(`Xulosam $  {userxulosa}`);
+
+// console.log(
+//   `Mening ismim ${userName}.
+//   Mening familyam ${userfamilya}.
+//   .Men ${userAge}daman.
+//   Guruxim ${userGroup}.
+//   Telefon raqamim ${userTell}.
+//   emailadres ${userEmail}.
+//   Jinsim ${userMW}.
+//   My favourite color ${userColor}.
+//   Xulosam ${userxulosa}`
+// );
